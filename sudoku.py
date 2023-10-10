126dd38acd1856ae48c2de6f2a9fbbca2978addb
natasia noble <natasia.noble@gmail.com>
2023-10-10 20:20:37 +0200

    headache solved by frankensteining mine, Matias, and Marc's code

--- sudoku.py
+++ sudoku.py
@@ -1,22 +1,16 @@
-# pylint: disable=missing-docstring
-
-
-# def sudoku_validator(grid):
-#     counter = 0
-#     for row in grid[counter]:
-#         if sorted(row) == [1,2,3,4,5,6,7,8,9]:
-#             counter += 1
-#     if counter > 8:
-#         print("Rows are okay")
-
-
-def check_sudoku(grid):
-    """ Return True if grid is a valid Sudoku square, otherwise False. """
-    for i in range(9):
+'''hello'''
+def sudoku_validator(grid):
+    """ Return True if grid is a valid Sudoku square, otherwise False."""
+    for i in range(9): #i will be your index
         # j, k index top left hand corner of each 3x3 tile
-        j, k = (i // 3) * 3, (i % 3) * 3
-        if len(set(grid[i,:])) != 9 or len(set(grid[:,i])) != 9 or len(set(grid[j:j+3, k:k+3].ravel())) != 9:
+        j = (i // 3) * 3 #the first run will be 0 obvs
+        k = (i % 3) * 3
+
+        # Check row, column, and 3x3 subgrid for duplicates
+        if (
+            len(set(grid[i])) != 9
+            or len(set(row[i] for row in grid)) != 9 #
+            or len(set(grid[row][col] for row in range(j, j + 3) for col in range(k, k + 3))) != 9
+        ):
             return False
     return True
-
-'''https://scipython.com/book/chapter-6-numpy/examples/checking-a-sudoku-grid-for-validity/'''
